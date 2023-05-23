e3a8a53195004bb1b331bdd16d94fff48f69bc02
Mark Czarnetzki <mark.czarnetzki@linovy.de>
2023-05-23 14:24:23 +0200

    Initial comit

---
+++ src/routes/+page.svelte
@@ -0,0 +1,18 @@
+<script>
+import Chart from "./Chart.svelte";
+</script>
+
+<svelte:head>
+    <title>Home</title>
+    <meta name="description" content="Svelte demo app"/>
+</svelte:head>
+
+<section>
+    <h1>Hi</h1>
+    <Chart/>
+</section>
+<style>
+    h1 {
+        font-size: 12px;
+    }
+</style>
